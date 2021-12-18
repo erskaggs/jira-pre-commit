7250503c8c62cdb5854ec5451964ce370324c10a
erskaggs <eric.skaggs@evercast.us>
2021-12-17 21:00:05 -0600

    fix: fixing errror msg

--- jira-pre-commit.sh
+++ jira-pre-commit.sh
@@ -5,7 +5,7 @@ develop_branch="develop"
 current_branch="$(git rev-parse --abbrev-ref HEAD)"
 # regex to validate in commit msg
 commit_regex='([A-Z]+-[0-9])'
-error_msg="Aborting commit. Your commit message is missing either a JIRA Issue, i.e. JIRA-1234."
+error_msg="Aborting commit. Your commit message is missing either a JIRA Issue, i.e. JIRA-1234, or the jira key is not IN ALL CAPS."
 
 commitTitle="$(cat $1 | head -n1)"
 
